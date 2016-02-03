13967a1c42a8129eb94ff507d1fb823e773c3d94
pdean1 <pdean1@my.westga.edu>
2016-02-03 01:27:49 -0500

    Fixing a bug causing the app to crash

--- app/src/main/java/cs6242/westga/edu/patrickdeanactivities/DisplayAllMessages.java
+++ app/src/main/java/cs6242/westga/edu/patrickdeanactivities/DisplayAllMessages.java
@@ -1,11 +1,14 @@
 package cs6242.westga.edu.patrickdeanactivities;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 public class DisplayAllMessages extends AppCompatActivity {
 
@@ -16,7 +19,7 @@ public class DisplayAllMessages extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-       FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -25,6 +28,17 @@ public class DisplayAllMessages extends AppCompatActivity {
             }
         });
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+        TextView textView = (TextView) findViewById(R.id.textView);
+        textView.setText(message);
+        message = intent.getStringExtra(DisplayMessageActivity.EXTRA_MESSAGE2);
+        TextView textView2 = (TextView) findViewById(R.id.textView);
+        textView2.setText(message);
+        RelativeLayout rl = (RelativeLayout) findViewById(R.id.container);
+        rl.addView(textView);
+        rl.addView(textView2);
+
     }
 
 }
--- app/src/main/java/cs6242/westga/edu/patrickdeanactivities/DisplayMessageActivity.java
+++ app/src/main/java/cs6242/westga/edu/patrickdeanactivities/DisplayMessageActivity.java
@@ -1,75 +1,41 @@
 package cs6242.westga.edu.patrickdeanactivities;
 
-import android.app.Fragment;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.LayoutInflater;
-import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 public class DisplayMessageActivity extends AppCompatActivity {
-
+    public final static String EXTRA_MESSAGE2 = "edu.westga.cs6242.patrickactivities.MESSAGE2";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_message);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null)
-                        .show();
-            }
-        });
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-
         Intent intent = getIntent();
         String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
-        TextView textView = new TextView(this);
+        TextView textView = (TextView) findViewById(R.id.message1);
         textView.setTextSize(40);
         textView.setText(message);
 
-        RelativeLayout layout = (RelativeLayout) findViewById(R.id.container);
+        RelativeLayout layout = (RelativeLayout) findViewById(R.id.holder);
         layout.addView(textView);
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle app bar item clicks here. The app bar
-        // automatically handles clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-
-        public PlaceholderFragment() { }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_display_message,
-                    container, false);
-            return rootView;
-        }
+    public void showMessages(View v) {
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        TextView textView = (TextView) findViewById(R.id.message1);
+        EditText editText = (EditText) findViewById(R.id.message2);
+        String message = textView.getText().toString();
+        intent.putExtra(MainActivity.EXTRA_MESSAGE, message);
+        message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE2, message);
+        startActivity(intent);
     }
 
 }
